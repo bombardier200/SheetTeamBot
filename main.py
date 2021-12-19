a8e25dbb10be4f06ccb4d2f57fe876a1b30a9397
bombardier200 <johnathan.baird02@gmail.com>
2021-12-19 18:19:11 -0500

    Redid data struct and embed, also working on getting json loading and saving once the bot gets closed.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 from discord.ext.commands import MissingRole
+import atexit
+import json
+import random
 from discord import embeds
 from dotenvy import load_env,read_file
 
 import os
-import asyncio
 load_env(read_file('.env'))
 TOKEN=os.getenv('DISCORD_TOKEN')
 class sheetteam(commands.Cog):
@@ -14,35 +16,35 @@ class sheetteam(commands.Cog):
         self.bot=bot;
         self.channel=None;
         self.testarray={};
-    """
-    @client.event
-    async def on_ready():
-        print('We have logged in as {0.user}'.format(client))
-    
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-    
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-    """
     @commands.command()
-    async def ping(ctx):
+    async def ping(self,ctx):
+        print(self.testarray[0]["guildName"])
         await ctx.reply("this worked")
     @commands.command()
-    async def request(self,ctx,request,sheetLink):
-        self.testarray[len(self.testarray)+1]={request,str(sheetLink)};
+    async def request(self,ctx,guildName,request,sheetLink):
+        #+random.randint(1,20)
+        ticketNumber= len(self.testarray)
+        self.testarray[ticketNumber]={"guildName":guildName,"request":request,"Sheet Link":str(sheetLink)};
         searchRole = get(ctx.guild.roles,name="testRole")
+        embed = discord.Embed(title="Sample Embed", description="This is a test")
+        embed.set_author(name="Sheet Bot", icon_url=ctx.author.avatar_url)
+        embed.add_field(name="Request Number", value=ticketNumber)
+        embed.add_field(name="Guild Name", value=guildName)
+        embed.add_field(name="Issue", value=request)
+        embed.add_field(name="Sheet Link", value=sheetLink)
+        await ctx.send(embed=embed)
         await ctx.send(f"Added request for sheet team {searchRole.mention}");
 
     @commands.command()
     @commands.has_role("admin")
     async def seerequest(self,ctx):
         quote_text=''
+        embed=discord.Embed(title="Current Requests", description="This is the current requests for sheet team")
+        embed.set_author(name="Sheet Bot")
         for elements in self.testarray:
-            quote_text= quote_text +str(self.testarray[elements])+'\n';
-        await ctx.reply(str(quote_text));
+            embed.add_field(name="Guild Name",value=self.testarray[elements]["guildName"])
+            embed.add_field(name="Ticket Number",value=elements,inline=False)
+        await ctx.send(embed=embed)
     @seerequest.error
     async def seerequest_error(self, ctx, error):
         if isinstance(error, MissingRole):
@@ -58,15 +60,14 @@ class sheetteam(commands.Cog):
         if isinstance(error, MissingRole):
             await ctx.send("Sorry you do not have permission for that command");
     """
-    @commands.command()
-    async def testembed(self,ctx):
-        embed=discord.Embed(title="Sample Embed",description="This is a test")
-        embed.set_author(name="Sheet Bot",icon_url=ctx.author.avatar_url)
-        embed.add_field(name="Request Number",value="1")
-        embed.add_field(name="Guild Name",value="Guild name")
-        embed.add_field(name="Issue",value="issue")
-        embed.add_field(name="sheet link",value="sheet link")
-        await ctx.send(embed=embed)
+data={}
+with open("data.json","r") as file:
+    for key,value in json.load(file).items():
+        data[key]=value
+def on_close():
+    with open("data.json","w") as file:
+        json.dump(data,file,indent=3)
+        print("Data is dumped")
 bot=commands.Bot(command_prefix='$')
 cogs = [sheetteam(bot)]
 for cog in cogs:
