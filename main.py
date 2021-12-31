3b062ce891ee915bb2f3cc7d20292510bf1bfcbe
bombardier200 <johnathan.baird02@gmail.com>
2021-12-31 10:12:04 -0600

    Got slash command working, now starting to change all commands over and test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import discord
+from discord_ui import Button, UI
 from discord import Intents
 from discord.ext import commands
 from discord.utils import get
 from discord.ext.commands import MissingRole
 from discord_slash import SlashCommand,cog_ext,SlashContext
+from discord_slash.utils.manage_commands import create_choice,create_option
 import atexit
 import json
 import random
@@ -17,11 +19,24 @@ class sheetteam(commands.Cog):
         self.bot=bot;
         self.channel=None;
         self.testarray=data;
-    @cog_ext.cog_slash(name="test",description="test1")
-    async def _test(self,ctx:SlashContext):
-        await ctx.send("pong")
-    @cog_ext.cog_slash(name="request")
-    async def _request(self,ctx:SlashContext,*args):
+    test_guilds=[683572714564222978]
+    @cog_ext.cog_slash(
+        name="ping",
+        description="Ping test",
+        guild_ids=test_guilds,
+        options=[
+            create_option(
+                name="Guild Name",
+                description="Enter the guild name",
+                required=True,
+                option_type=3
+            )
+        ]
+    )
+    async def _ping(self,ctx:SlashContext,option:str):
+        await ctx.send(option)
+    @commands.command()
+    async def request(self,ctx:SlashContext,*args):
         guildName=args[0]
         request=" ".join(args[1:len(args)-1])
         sheetLink=args[len(args)-1]
@@ -36,7 +51,6 @@ class sheetteam(commands.Cog):
         embed.add_field(name="Sheet Link", value=sheetLink)
         await ctx.send(embed=embed)
         await ctx.send(f"Added request for sheet team {searchRole.mention}");
-
     @commands.command()
     async def seerequest(self,ctx):
         if ctx.channel.id==683572715025596428:
@@ -68,11 +82,12 @@ class sheetteam(commands.Cog):
         if isinstance(error, MissingRole):
             await ctx.send("Sorry you do not have permission for that command");
     """
+    """
     @commands.command()
     async def hello(self,ctx,name:str=None):
         name=name or ctx.author.name
         await ctx.respond(f"hello {name}!")
-
+    """
 data={}
 with open("data.json","r") as file:
     for key,value in json.load(file).items():
